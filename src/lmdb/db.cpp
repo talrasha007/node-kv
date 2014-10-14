8e2c7b87495eec9162c6fb80d686e24b457e2245
Anlv.Tan <anlv.tan@tendcloud.com>
2014-10-14 11:34:15 +0800

    move class name to export function, fix a strange issue in msvc.

--- src/lmdb/db.cpp
+++ src/lmdb/db.cpp
@@ -14,17 +14,13 @@ void kv::lmdb::setup_db_export(v8::Handle<v8::Object>& exports) {
 	KV_TYPE_EACH(DB_EXPORT);
 }
 
-#define DB_CLASS_NAME(KT, VT) template <> std::string db<KT, VT>::class_name(std::string("DB_") + KT::type_name + "_" + VT::type_name);
-namespace kv {
-	namespace lmdb {
-		KV_TYPE_EACH(DB_CLASS_NAME)
-	}
-}
-
 template <class K, class V> void db<K, V>::setup_export(Handle<Object>& exports) {
+	char class_name[64];
+	sprintf(class_name, "DB_%s_%s", K::type_name, V::type_name);
+
 	// Prepare constructor template
 	Local<FunctionTemplate> dbiTpl = NanNew<FunctionTemplate>(db::ctor);
-	dbiTpl->SetClassName(NanNew(db::class_name.c_str()));
+	dbiTpl->SetClassName(NanNew(class_name));
 	dbiTpl->InstanceTemplate()->SetInternalFieldCount(1);
 
 	// Add functions to the prototype
@@ -32,7 +28,7 @@ template <class K, class V> void db<K, V>::setup_export(Handle<Object>& exports)
 	// TODO: wrap mdb_stat too
 
 	// Set exports
-	exports->Set(NanNew(db::class_name.c_str()), dbiTpl->GetFunction());
+	exports->Set(NanNew(class_name), dbiTpl->GetFunction());
 }
 
 template <class K, class V> NAN_METHOD((db<K, V>::ctor)) {
