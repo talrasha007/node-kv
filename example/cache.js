78a95f5c247b65c9bb168702e5e2c7967acb7a73
Tal <talrasha007@gmail.com>
2014-10-19 19:48:34 +0800

    cache example.

--- example/cache.js
+++ example/cache.js
@@ -1,1 +1,25 @@
-var cache = require('../').cache;
+var path = require('path'),
+    cache = require('../').cache;
+
+var cenv = new cache.Env({
+    dir: path.join(__dirname, 'testdb', 'cache'),
+    cacheSize: 128 * 1024 * 1024,   // 256M by default
+    batchSize: 128                  // 64 by default
+});
+
+var cdb = cenv.openDb({
+    name: 'testdb',
+    keyType: 'int32',
+    valType: 'int32'
+});
+
+cdb.put(1, 2);
+cdb.put(2, 3);
+cenv.flushBatchOps(); // Data will flushed automatically after 1ms, if you want to query immediately, do this.
+console.log(cdb.get(1));
+
+cdb.put(3, 3);
+setTimeout(function () {
+    console.log(cdb.get(3));
+    cenv.close();
+}, 50);
