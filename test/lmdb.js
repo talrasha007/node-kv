de0c35ff709b6c89d66505083820caf6d81ef450
Tal <talrasha007@gmail.com>
2014-10-26 02:40:20 +0800

    cursor test.

--- test/lmdb.js
+++ test/lmdb.js
@@ -19,7 +19,7 @@ describe('LMDB int32-int32', function () {
     var dupdb = env.openDb({
         name: 'testdup',
         keyType: 'int32',
-        valType: 'int32',
+        valType: 'hex',
         allowDup: true
     });
 
@@ -73,23 +73,73 @@ describe('LMDB int32-int32', function () {
     });
 
     it('dup should work as expected.', function () {
-        dupdb.put(1, 1);
-        expect(dupdb.exists(1, 1)).to.be(true);
-        expect(dupdb.exists(1, 2)).to.be(false);
-        dupdb.put(1, 2);
-        expect(dupdb.exists(1, 1)).to.be(true);
-        expect(dupdb.exists(1, 2)).to.be(true);
+        dupdb.put(1, '1adb');
+        expect(dupdb.exists(1, '1adb')).to.be(true);
+        expect(dupdb.exists(1, '2adb')).to.be(false);
+        dupdb.put(1, '2adb');
+        expect(dupdb.exists(1, '1adb')).to.be(true);
+        expect(dupdb.exists(1, '2adb')).to.be(true);
 
         var txn = env.beginTxn();
-        dupdb.put(2, 1, txn);
-        expect(dupdb.exists(2, 1, txn)).to.be(true);
-        expect(dupdb.exists(2, 1)).to.be(false);
-        dupdb.put(2, 2, txn);
-        expect(dupdb.exists(2, 2, txn)).to.be(true);
-        expect(dupdb.exists(2, 2)).to.be(false);
+        dupdb.put(2, '1adb', txn);
+        expect(dupdb.exists(2, '1adb', txn)).to.be(true);
+        expect(dupdb.exists(2, '1adb')).to.be(false);
+        dupdb.put(2, '2adb', txn);
+        expect(dupdb.exists(2, '2adb', txn)).to.be(true);
+        expect(dupdb.exists(2, '2adb')).to.be(false);
         txn.commit();
-        expect(dupdb.exists(1, 1)).to.be(true);
-        expect(dupdb.exists(1, 2)).to.be(true);
+        expect(dupdb.exists(2, '1adb')).to.be(true);
+        expect(dupdb.exists(2, '2adb')).to.be(true);
+    });
+
+    it('cursor should work as expected.', function () {
+        var i, j;
+        for (i = 100; i < 110; i++) {
+            db.put(i, i);
+            for (j = 0; j < 10; j += 3) {
+                dupdb.put(i, j + 'abc');
+                dupdb.put(i, j + 'abc');
+            }
+        }
+
+        var txn = env.beginTxn();
+        var cur = db.cursor(txn), dupcur = dupdb.cursor(txn);
+        expect(function () { cur.current(); }).to.throwException();
+        expect(function () { cur.seek(100, 100); }).to.throwException();
+        for (i = 100; i < 101; i++) {
+            expect(cur.seek(i)).to.eql([i, i]);
+            for (j = 0; j < 10; j++) {
+                if (0 === j % 3) expect(dupcur.seek(i, j + 'abc')).to.eql([i, j + 'abc']);
+                else expect(dupcur.seek(i, j + 'abc')).to.be(null);
+            }
+        }
+
+        expect(cur.seek(99)).to.be(null);
+        expect(cur.lowerBound(99)).to.eql([100, 100]);
+        expect(cur.lowerBound(106)).to.eql([106, 106]);
+        expect(cur.seek(111)).to.be(null);
+        expect(dupcur.lowerBound(99)).to.eql([100, '0abc']);
+        expect(dupcur.lowerBound(99, '000')).to.be(null);
+        expect(dupcur.lowerBound(100, '0abc')).to.eql([100, '0abc']);
+        expect(dupcur.lowerBound(100, '1abc')).to.eql([100, '3abc']);
+        expect(dupcur.lowerBound(100, '2abc')).to.eql([100, '3abc']);
+        expect(dupcur.lowerBound(100, '3abc')).to.eql([100, '3abc']);
+
+        expect(dupcur.seek(100)).to.eql([100, '0abc']);
+        expect(dupcur.nextDup()).to.eql([100, '3abc']);
+        expect(dupcur.nextDup()).to.eql([100, '6abc']);
+        expect(dupcur.nextDup()).to.eql([100, '9abc']);
+        expect(dupcur.nextDup()).to.be(null);
+
+        expect(dupcur.seek(100)).to.eql([100, '0abc']);
+        expect(dupcur.next()).to.eql([100, '3abc']);
+        expect(dupcur.next()).to.eql([100, '6abc']);
+        expect(dupcur.next()).to.eql([100, '9abc']);
+        expect(dupcur.next()).to.eql([101, '0abc']);
+
+        cur.close();
+        dupcur.close();
+        txn.commit();
     });
 
     after(function () {
