878ded6c6992ba48ee1522d4f21391e2b5df749c
Anlv.Tan <anlv.tan@tendcloud.com>
2014-11-18 11:10:41 +0800

    RWMutex impl.  CondVar::TimedWait impl.

--- 3rd-party/leveldb/port-libuv-rocksdb/port_uv.cc
+++ 3rd-party/leveldb/port-libuv-rocksdb/port_uv.cc
@@ -12,7 +12,7 @@
 namespace rocksdb {
 namespace port {
 
-Mutex::Mutex() { uv_mutex_init(&mu_); }
+Mutex::Mutex(bool) { uv_mutex_init(&mu_); }
 
 Mutex::~Mutex() { uv_mutex_destroy(&mu_); }
 
@@ -20,23 +20,25 @@ void Mutex::Lock() { uv_mutex_lock(&mu_); }
 
 void Mutex::Unlock() { uv_mutex_unlock(&mu_); }
 
-RWMutex::RWMutex() { uv_mutex_init(&mu_); }
+RWMutex::RWMutex() { uv_rwlock_init(&mu_); }
 
-RWMutex::~RWMutex() { uv_mutex_destroy(&mu_); }
+RWMutex::~RWMutex() { uv_rwlock_destroy(&mu_); }
 
-void RWMutex::ReadLock() { uv_mutex_lock(&mu_); }
+void RWMutex::ReadLock() { uv_rwlock_rdlock(&mu_); }
 
-void RWMutex::WriteLock() { uv_mutex_lock(&mu_); }
+void RWMutex::WriteLock() { uv_rwlock_wrlock(&mu_); }
 
-void RWMutex::ReadUnlock() { uv_mutex_unlock(&mu_); }
+void RWMutex::ReadUnlock() { uv_rwlock_rdunlock(&mu_); }
 
-void RWMutex::WriteUnlock() { uv_mutex_unlock(&mu_); }
+void RWMutex::WriteUnlock() { uv_rwlock_wrunlock(&mu_); }
 
 CondVar::CondVar(Mutex* mu) : mu_(mu) { uv_cond_init(&cv_); }
 
 CondVar::~CondVar() { uv_cond_destroy(&cv_); }
 
 void CondVar::Wait() { uv_cond_wait(&cv_, &mu_->mu_); }
+
+bool CondVar::TimedWait(uint64_t abs_time_us) { return ETIMEDOUT == uv_cond_timedwait(&cv_, &mu_->mu_, abs_time_us); }
 
 void CondVar::Signal() { uv_cond_signal(&cv_); }
 
