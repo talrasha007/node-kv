8fef0e7ec14341371d13d326e1db8a21553993b0
Anlv.Tan <anlv.tan@tendcloud.com>
2014-11-18 19:41:31 +0800

    fix missing build_version.cc

---
+++ 3rd-party/leveldb/leveldb-rocksdb-3.7/util/build_version.cc
@@ -0,0 +1,6 @@
+#include "build_version.h"
+
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:9fe22486149b89bc6c13d3a4473568d9296b4965";
+
+const char* rocksdb_build_compile_time = __TIME__;
+const char* rocksdb_build_compile_date = __DATE__;
