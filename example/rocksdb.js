9fe5c5b0df8b71ddd8c630e4e9d6c673caa5f134
Anlv.Tan <anlv.tan@tendcloud.com>
2014-11-19 13:27:52 +0800

    rocksdb lib.

---
+++ example/rocksdb.js
@@ -0,0 +1,29 @@
+var path = require('path'),
+    rocksdb = require('../').rocksdb;
+
+var env = new rocksdb.Env({
+    dir: path.join(__dirname, 'testdb', 'rocks'),
+    cacheSize: 256 * 1024 * 1024 // 4MB by default.
+});
+
+var db = env.openDb({
+    name: 'test',
+    keyType: 'int32',
+    valType: 'int32'
+});
+
+db.put(1, 1);
+console.log(db.get(1));
+db.del(1);
+console.log(db.get(1));
+
+db.del(3);
+db.batchPut(3, 4);
+console.log(db.get(3));
+db.flushBatchOps();
+console.log(db.get(3));
+
+var cur = db.cursor();
+for (var i = cur.first(); i; i = cur.next()) {
+    console.log([cur.key(), cur.val()]);
+}
